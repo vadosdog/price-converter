d96728a503f403c6e3b22558985a5475182dd731
vadosdog <vados.sparx@gmail.com>
2019-11-29 15:47:43 +0300

    fix

--- App/Converter.php
+++ App/Converter.php
@@ -125,25 +125,20 @@ class Converter
 						$nextOrderPriceNode = $nextOrderNode->getValue();
 						$nextOrderPrice = $nextOrderPriceNode->getPrice();
 
-						/*
-						 * Если delivery_date_from раньше текущей, добавляем еще одну строку
-						 */
-						if ($nextOrderPrice->delivery_date_from <= $currentPrice->delivery_date_from) {
-							/** @var PriceNode $prevOrderPriceNode */
-							$prevOrderPriceNode = $nextOrderNode->prev()->getValue();
-							$additionalRow = new PriceResource(
-								$position_id,
-								$nextOrderPrice->price,
-								$nextOrderPrice->order_date_from,
-								$row->getDeliveryDateFrom(),
-								$prevOrderPriceNode->getPrice()->order_date_from,
-								null
-							);
-							if ($row->getDeliveryDateTo()) {
-								$additionalRow->setConvertedDeliveryDateTo($row->getDeliveryDateTo());
-							}
-							$result->add($additionalRow);
+						/** @var PriceNode $prevOrderPriceNode */
+						$prevOrderPriceNode = $nextOrderNode->prev()->getValue();
+						$additionalRow = new PriceResource(
+							$position_id,
+							$nextOrderPrice->price,
+							$nextOrderPrice->order_date_from,
+							$row->getDeliveryDateFrom(),
+							$prevOrderPriceNode->getPrice()->order_date_from,
+							null
+						);
+						if ($row->getDeliveryDateTo()) {
+							$additionalRow->setConvertedDeliveryDateTo($row->getDeliveryDateTo());
 						}
+						$result->add($additionalRow);
 					}
 				}
 			}
